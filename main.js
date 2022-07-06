6c669da9da30a8458239286717a3628b4b6005dc
Okpe onoja <67971490+onoja123@users.noreply.github.com>
2022-07-06 02:44:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 
+alert("Welcome to moving item using keypad")
+
+//get design element
+let yu = document.getElementById("lokl")
+
+//keypress listener
+window.addEventListener("keydown", move);
+let x = 0;
+let y = 0;
+
+//move function
+function move(event){
+    switch (event.key) {
+        case "ArrowDown":
+        y+=5;
+        yu.style.top = y + "px"  
+            break;
+    
+            case "ArrowUp":
+                y-=5;
+                yu.style.top = y + "px"  
+                    break;
+
+                    case "ArrowRight":
+                        y+=5;
+                        yu.style.left = y + "px"  
+                            break;
+
+                            case "ArrowLeft":
+                                y-=5;
+                                yu.style.left = y + "px"  
+                                    break;
+        default:
+            break;
+    }
+
+}
